3a09559eeed3981a4d5df2b7ca50a7c673fc6b89
Amy Wilson <amy.liz@me.com>
2017-07-19 09:14:53 -0700

    completion of headings branch

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     var headings = $('h2');
-    headings[3].innerText = "I'm Amy Wilson, and I'm learning jQuery";
+    headings[3].innerText = "I'm Amy Wilson, and I'm learning jQuery!";
 
 });
